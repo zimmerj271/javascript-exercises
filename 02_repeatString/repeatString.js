d014f1ead7e5c0b855bf85dd25044ba92310e7f6
Justin Zimmerman <zimmerj271@pm.me>
2022-02-15 18:23:22 -0700

    Completed homework 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return str.repeat(num);
+    }   
 };
 
 // Do not edit below this line
