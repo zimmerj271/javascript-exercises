8c90f02d08b201500030744559e9d96c7889a46c
Justin Zimmerman <zimmerj271@pm.me>
2022-02-16 07:54:36 -0700

    Completed reverseString assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const reversed = []; // define array to contain chars in reverse order
+    /* loop through string and push chars in reverse order */
+    for (const i in str) { 
+        reversed.push( str.charAt(str.length - 1 - i) ) 
+    }
 
+    return reversed.join(''); // join array with empty char
 };
 
 // Do not edit below this line
