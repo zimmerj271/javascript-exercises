1e2704d8ac38a99f7e79b7dd4ff611dd67c51f7d
Justin Zimmerman <zimmerj271@pm.me>
2022-02-16 19:17:21 -0700

    Completed assignments 4, 5, and 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0].slice(); // copy input array to arr
+    // loop through arguments of the function
+    for (let i = 1; i < arguments.length; i++) { // loop through remaining function parameters
+        for (let j = 0; j < arr.length; j++) { // loop through the array in first parameter position
+            if(arguments[i] === arr[j]) {
+                arr.splice(j,1); // if values are equal then remove from input array
+            }
+        }
+    }
+    return arr;
 };
 
-// Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((num1 < 0) || (num2 < 0)) { // if parameters are negative throw error
+        return "ERROR";
+    } else if ((typeof(num1) != 'number') || (typeof(num2) != 'number')) { // if parameters are not numbers throw error
+        return "ERROR";
+    } else {
+        let sum = 0; // instantiate sum
+        /* make sure looping is done from small to big despite parameter input */
+        let big, small;
+        if (num2 > num1) {
+            big = num2;
+            small = num1;
+        } else {
+            big = num1;
+            small = num2;
+        }
 
+        // loop from small to big and sum each value 
+        for (let i = small; i <= big; i++) { 
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
