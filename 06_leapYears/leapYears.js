6c9b50ea151496e5ee1abf9ec9d91daa9fe1043d
Justin Zimmerman <zimmerj271@pm.me>
2022-02-18 07:36:05 -0700

    Completed assignments 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,12 @@
 const leapYears = function(year) {
-    if((year % 4 === 0) || ( (year % 100 === 0) && (year % 400 === 0) )) {
+    
+    if((year % 4 === 0) && (year % 100 !== 0) ) { // years that are divisible by 4 but not divisible by 100
         return true;
-    } else {
+    } else if ( (year % 100 === 0) && (year % 400 === 0) ) { // years that are divisibe by 100 and divisible by 400
+        return true;
+    } else {  // all other years
         return false;
     }
 };
 
-// Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(temp) { // function to convert Farhenheit to Celsius
+  let celsius = 5 * (temp - 32) / 9;
+  return round(celsius, 1);
+  
 };
 
-const ctof = function() {
-
+const ctof = function(temp) { // function to convert Celsious to Farenheit
+  let farenheit = 9 * temp / 5 + 32;
+  return round(farenheit, 1);
 };
+
+const round = function(num, precision=0) { // function to round number to nearest digit
+  var factor = Math.pow(10, precision || 0);
+  return Math.round(num * factor) / factor;
+}
 
 // Do not edit below this line
 module.exports = {
