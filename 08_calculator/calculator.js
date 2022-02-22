46bfcfd3536238b035574206a407b87540c62970
Justin Zimmerman <zimmerj271@pm.me>
2022-02-21 20:35:51 -0700

    Completed assignment 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let x in arr) {
+    sum += arr[x]
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiple = 1;
+  for (let x in arr) {
+    multiple *= arr[x];
+  }
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else {
+    const arr = [...Array(num).keys()].map(x => ++x); // generate array from 1 to num
+    return multiply(arr)
+  }
 };
 
 // Do not edit below this line
