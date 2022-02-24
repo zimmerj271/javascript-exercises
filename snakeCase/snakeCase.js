707f3525a2eba0253ee42d06ed9c869301fed4b2
Justin Zimmerman <zimmerj271@pm.me>
2022-02-23 18:16:59 -0700

    Completed snakeCase assignment

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,16 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+    str = str.replace(/[\s-]+/g, '_'); // convert white space and '-' to '_'
+    if (/[a-z]{3,}[A-Z]/.test(str)) {
+        str = str.replace(/([a-z])([A-Z])/g, '$1_$2' );    
+    }
+    str = str.replace(/([A-Z]+)/g, function(match) { // convert upper case to lower case
+        return match.toLowerCase();
+    });
+    str = str.replace(/\.{2,}$/, ''); // remove 2 or more '.' at end of string, must go before replacing '.' everywhere else
+    str = str.replace(/\.{2,}/g, '_'); // replace 2 or more '.' with '_'
+    str = str.replace(/[\.!\?,]+/g, ''); // remove any remaining punctuation
+    console.log(str);
+    return str;
 };
 
 // Do not edit below this line
